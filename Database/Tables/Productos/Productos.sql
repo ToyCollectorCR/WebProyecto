1ccbef47f936c16260aee46aadba00f2cad800e9
ToyCollectorCR <fdsega@gmail.com>
2021-04-18 22:57:19 -0600

    Ajustes a Tbla Productos

--- Database/Tables/Productos/Productos.sql
+++ Database/Tables/Productos/Productos.sql
@@ -3,7 +3,7 @@
 	IdProductos INT NOT NULL IDENTITY(1,1) CONSTRAINT PK_Productos PRIMARY KEY CLUSTERED(IdProductos),
 	NombreProductos VARCHAR(50) NOT NULL,
 	SesionesRayosUVA INT,
-	RenovacionCuota DATE,
+	RenovacionCuota DATETIME,
 	ProductosConsumidos VARCHAR(50) NOT NULL,
 	CompraProveedores VARCHAR(50) NOT NULL,
 	EstadoProducto BIT NOT NULL CONSTRAINT DF_Producto_EstadoProducto DEFAULT(0),
