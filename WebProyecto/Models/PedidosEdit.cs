6a0734e452a9f9dbd9011f21b66ba98298a5c299
Adriana Agüero <81051391+adriagov@users.noreply.github.com>
2021-04-28 15:27:25 -0600

    Index de pedidos funciona, Edit no lo muestra ADRI

---
+++ WebProyecto/Models/PedidosEdit.cs
@@ -0,0 +1,15 @@
+﻿using Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebProyecto.Models
+{
+    public class PedidosEdit
+    {
+        public PedidosEntity pedidos { get; set; }
+        public List<ProveedoresEntity> ddlproveedores { get; set; }
+        public List<ProductosEntity> ddlproductos { get; set; }
+    }
+}
